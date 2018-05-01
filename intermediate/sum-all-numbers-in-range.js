4214e656e97e43c12eec1bcf85889c1d64af2856
Your Name <maksim.fomenok@gmail.com>
2018-05-02 00:01:29 +0300

    Added sum all numbers in range algorithm

---
+++ intermediate/sum-all-numbers-in-range.js
@@ -0,0 +1,22 @@
+function sumAll(arr) {
+    var min = Math.min(...arr
+)
+    ;
+    var max = Math.max(...arr
+)
+    ;
+
+    var numbers = [];
+
+    for (var i = min; i <= max; ++i) {
+        numbers.push(i);
+    }
+
+    var sum = numbers.reduce(function (previus, current) {
+        return previus + current;
+    }, 0);
+
+    return sum;
+}
+
+sumAll([1, 4]);
